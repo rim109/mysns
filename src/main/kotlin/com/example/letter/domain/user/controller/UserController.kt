7d94061c43112c708e1f187432efb350cf5c44dd
rim109 <fla7722@naver.com>
2024-02-05 22:21:29 +0900

    [feat] user 단건 조회 코드 수정

--- src/main/kotlin/com/example/letter/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/example/letter/domain/user/controller/UserController.kt
@@ -1,5 +1,6 @@
 package com.example.letter.domain.user.controller
 
+import com.example.letter.common.security.jwt.UserPrincipal
 import com.example.letter.domain.user.dto.LoginRequest
 import com.example.letter.domain.user.dto.LoginResponse
 import com.example.letter.domain.user.dto.SignupRequest
@@ -9,11 +10,9 @@ import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class UserController(
@@ -37,8 +36,12 @@ class UserController(
     }
 
     @Operation(summary = "user 단건 조회")
+    @PreAuthorize("hasRole('ADMIN') or #userPrincipal.id == #userId")
     @GetMapping("/users/{userId}")
-    fun getUser(@PathVariable userId: Long): ResponseEntity<UserResponse> {
+    fun getUser(
+        @PathVariable userId: Long,
+        @AuthenticationPrincipal userPrincipal: UserPrincipal
+    ): ResponseEntity<UserResponse> {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUser(userId))
     }
 }
